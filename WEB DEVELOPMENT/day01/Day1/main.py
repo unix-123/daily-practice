134d67bf97cb3c0eed9085779a01d9deee16eedf
Aayush <aayushdutta13@gmail.com>
2026-08-19 15:04:18 +0530

    initial daily practice setup

---
+++ WEB DEVELOPMENT/day01/Day1/main.py
@@ -0,0 +1,26 @@
+#FIRST PYTHON PROGRAM
+print("Hello, World!")
+# #COMMENTS, ESCAPE SEQUENCE, AND PRINT STATEMENT
+#this is just a simple comment of a good boy
+print("hey my name is Aayush \nUnix")
+print("hey my name is \"Aayush\" \nUnix")
+print("unix",69, 96)
+print("unix", 69, 96, sep="*", end="NULL")
+# #VARIABLES AND DATA TYPES
+a = 5
+b = True
+c = None
+d = "Unix"
+print("Type of a is", type(a))
+print("Type of b is", type(b))
+print("Type of c is", type(c))
+print("Type of d is", type(d))
+#LISTS AND TUPLES
+LIST = [1,2,1,5,5,"HEY","1.1"]
+print(LIST)
+tuple = (1,2,1,5,5,"HEY","1.1")
+print(tuple)
+dict={"name":"Aayush","age":20,"city":"Delhi"}
+print(dict)
+print("type of dict is", type(dict))
+print(dict)
