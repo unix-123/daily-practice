3a7aaeb3410ee00ac2b29d2e110b9874d44c277f
Aayush <aayushdutta13@gmail.com>
2026-08-22 16:37:24 +0530

    Day 3 Python practice

--- PYTHON/Day2/main.py
+++ PYTHON/Day2/main.py
@@ -1,5 +1,12 @@
 
-x = str(input("Enter a number: "))
-y = str(input("Enter a number: "))
-print(x + y)
-print(int(x) + int(y))
+# x = str(input("Enter a number: "))
+# y = str(input("Enter a number: "))
+# print(x + y)
+# print(int(x) + int(y))
+a=3
+b=4
+c=5
+print(int(a)+int(b)+int(c))
+d=2.3
+e=7
+print(d + e)
