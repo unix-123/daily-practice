45e527215cb6df19c521ffd7e55074e3dc4a12e6
Aayush <aayushdutta13@gmail.com>
2026-08-23 16:01:25 +0530

    Day 4 Python practice

--- PYTHON/day3/main.py
+++ PYTHON/day3/main.py
@@ -1,33 +1,33 @@
-# name = "UNIX, hey hello how are you doing today?"
-# subject = "python"
-# print("My name is " + name)
-# len1 = len(subject)
-# print(len1)
-# print(subject[0:4])
-# print(subject[-4:-2])
+name = "UNIX, hey hello how are you doing today?"
+subject = "python"
+print("My name is " + name)
+len1 = len(subject)
+print(len1)
+print(subject[0:4])
+print(subject[-4:-2])
 
-# a="UNIX here, how are you my friend?"
-# print(len(a))
-# print(a.upper())
-# print(a.lower())
-# print(a.replace("here","the great"))
-# print(a.split(" "))
-# print(a.count("e"))
-# print(a.find("o"))
-# print(a.isalnum())
-# print(a.isspace())
-# print(a.isprintable())
-# print(a.istitle())
-# print(a.title())
+a="UNIX here, how are you my friend?"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.replace("here","the great"))
+print(a.split(" "))
+print(a.count("e"))
+print(a.find("o"))
+print(a.isalnum())
+print(a.isspace())
+print(a.isprintable())
+print(a.istitle())
+print(a.title())
 
-# age = int(input("my age is: "))
-# print("my age is: " , age)
-# if age<=18:
-#     print("cannot drive")
-# elif age>=60:
-#     print("cannot drive")
-# else:
-#     print("can drive")
+age = int(input("my age is: "))
+print("my age is: " , age)
+if age<=18:
+    print("cannot drive")
+elif age>=60:
+    print("cannot drive")
+else:
+    print("can drive")
 
 import time
 timestamp=time.strftime("%H:%M:%S")
