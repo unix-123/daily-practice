70a199a1a63ee145fce151d8ac549e7ac5fceb4e
Aayush <aayushdutta13@gmail.com>
2026-08-23 16:01:25 +0530

    Day 4 Python practice

---
+++ PYTHON/day3/day4/main.py
@@ -0,0 +1,48 @@
+# colours=["red", "green", "blue"]
+# for colour in colours:
+#  print(colour)
+# for i in colour:
+#     print(i, end="  ")
+
+# for i in range(7):
+#     print(i+5)
+
+# for i in range(0, 15, 2):
+#     print(i)
+
+# i=0
+# while i<=4:
+#     print(i)
+#     i=i+2
+
+# i=int(input('enter the number:'))
+# while i<=10:
+#     i=int(input('enter the number:'))
+#     print(i)
+# print('THANKYOU..!')
+
+# i = 10
+# while i >=0:
+#     print(i)
+#     i = i -1
+# else: print("i'm done")
+
+# a=int(input("enter the 1stnumber:"))
+# b=int(input("enter the 2ndnumber:"))
+# def calculate_this(a,b):
+#     calculate=(a+b)/(a-b)
+#     print(calculate)
+# # a=10
+# # b=5
+# calculate_this(a,b)
+
+def isgreater(a,b):
+    if a>b:
+        print("a is greater than b")
+    elif a<b:
+        print("b is greater than a")
+    else:
+        print("both are equal")
+
+def issmaller(a,b):
+    pass
