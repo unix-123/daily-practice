a20e62d65509c022c9322a3cc2723dde6f89bb8b
Aayush <aayushdutta13@gmail.com>
2026-08-25 22:08:12 +0530

    day5 of python

---
+++ PYTHON/day5/main.py
@@ -0,0 +1,35 @@
+# def average(a, b):
+#     print("the average is:", (a + b) / 2)
+
+# average(10, 40)
+# average(a=20, b=30)
+
+# marks=[1,2,"unix",True,"hey",5,8,10]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(marks[2])
+# print(marks[3])
+# if 2 in marks:
+#     print("yes")
+# else:
+#     print('no')
+
+# if "nix" in "Unix":
+#     print("yes")
+# print(marks)
+# print(marks[0:len(marks)])
+# print(marks[:])
+# print(marks[1:-1])
+# print(marks[1:6])
+# print(marks[1:6:2])
+
+list=[i for i in range(10)]
+print(list)
+
+list=[i for i in range(10) if i%3==0]
+print(list)
+
+list=[i for i in range(10) if i%3!=0]
+print(list)
